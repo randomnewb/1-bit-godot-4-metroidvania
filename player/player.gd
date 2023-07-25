2e588bbda2d7ab045037d2ef3755e610a883c72e
randomnewb <nguy1581@gmail.com>
2023-07-25 16:52:39 -0500

    update player animation, add tileset

--- player/player.gd
+++ player/player.gd
@@ -7,6 +7,9 @@ extends CharacterBody2D
 @export var jump_force = 128;
 @export var max_fall_velocity = 128;
 
+@onready var animation_player = $AnimationPlayer;
+@onready var sprite_2d = $Sprite2D
+
 func _physics_process(delta):
 	apply_gravity(delta);
 	var input_axis = Input.get_axis("ui_left", "ui_right");
@@ -15,6 +18,7 @@ func _physics_process(delta):
 	else:
 		apply_friction(delta);
 	jump_check();
+	update_animations(input_axis);
 	move_and_slide();
 
 func is_moving(input_axis):
@@ -31,5 +35,19 @@ func apply_friction(delta):
 		velocity.x = move_toward(velocity.x, 0, friction * delta);
 
 func jump_check():
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = -jump_force;
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = -jump_force;
+	else:
+		if Input.is_action_just_released("ui_up") and velocity.y < -jump_force / 2:
+			velocity.y = -jump_force / 2;
+
+func update_animations(input_axis):
+	if is_moving(input_axis):
+		animation_player.play("run");
+		sprite_2d.scale.x = sign(input_axis);
+	else:
+		animation_player.play("idle");
+
+	if not is_on_floor():
+		animation_player.play("jump");
